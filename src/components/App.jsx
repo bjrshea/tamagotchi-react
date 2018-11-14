ca316da6e43b14ac99cf2774fb8b8ea98c32ba42
Student <example@example.com>
2018-11-14 13:17:53 -0800

    change component to statefull class, add masterStatList array in constructor state object, provide masterStatList as propType to TamagotchiStatsList tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import TamagotchiStatsList from './TamagotchiStatsList';
 import StatButtonList from './StatButtonList';
-import Moment from 'moment';
 
-function App() {
-  return (
-    <div>
-      <h1>Life of the Tamagotchi named Rielly</h1>
-      <StatButtonList/>
-      <img/>
-      <TamagotchiStatsList/>
-      <button>Play Again</button>
-      <button>Start</button>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      masterStatList: [
+        {
+          statId: 'feedStat',
+          statColor: '#00B12A',
+          statAmount: 100
+        },
+        {
+          statId: 'exerciseStat',
+          statColor: '#00A1B3',
+          statAmount: 100
+        },
+        {
+          statId: 'restStat',
+          statColor: '#DA003A',
+          statAmount: 100
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Life of the Tamagotchi named Rielly</h1>
+        <StatButtonList/>
+        <img/>
+        <TamagotchiStatsList
+          statList={this.state.masterStatList}
+          />
+        <button>Play Again</button>
+        <button>Start</button>
+      </div>
+    );
+  }
 }
 export default App;
